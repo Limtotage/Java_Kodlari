1d977d6b6de8bff42810a1f429ec6cc6f27a4333
Limtotage <abdullahltgyalim@gmail.com>
2024-10-19 09:12:55 +0300

    Update Daily_Solution_2.java

--- Leet_Code/Daily_Solution_2.java
+++ Leet_Code/Daily_Solution_2.java
@@ -20,3 +20,15 @@ class Daily_Solution_2 {
         return sip.charAt(k-1);
     }
 }
+class Daily_Solution_2_Recursive {
+    public char findKthBit(int n, int k) {
+        if (n == 1) return '0';
+        int mid = (1<<(n-1));
+        if(k==mid) return '1';
+        else if(k<mid) return findKthBit(n-1,k);
+        else {
+            char res =findKthBit(n,mid-(k-mid)); // ters çevrilmiş kısımdasın o yüzden bulduğun sonucu ters çeviriyoruz.
+            return res=='0'?'1':'0';  // result = 0 ise return 1 else return 0  
+        }
+    }
+}
